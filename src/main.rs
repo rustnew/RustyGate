bd5eeeefa95812997087727c111d7f3b1c4621b0
Fossouo  martial <martialwato@gmail.com>
2025-09-08 03:37:39 +0100

    tree commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+pub async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("RustyGate is running 🚀")
+}
+
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(health_check()))  
+    })
+    .bind("0.0.0.8080")?
+    .run()
+    .await
 }
